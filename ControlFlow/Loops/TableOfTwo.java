ea29b03157084a47960965dd3f36fab0928eaf17
Shridhar Aware <ashridhar19@gmail.com>
2024-03-03 12:19:55 +0530

    Table of number using for loop.

---
+++ ControlFlow/Loops/TableOfTwo.java
@@ -0,0 +1,10 @@
+// For loop
+
+class TableOfTwo{
+    public static void main(String[] args){
+        System.out.println("Table of two - ");
+        for(int i = 1; i < 11; i++){
+            System.out.println(2 * i);
+        }
+    }
+}
